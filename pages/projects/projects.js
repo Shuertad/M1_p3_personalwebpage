74b6a4e4669785bfd8c3dae9d60dd55849991e0d
shuertad <santibhs@yahoo.com>
2023-11-12 13:18:36 +0100

    update images locations to public for deployment

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -2,25 +2,25 @@ export const projects = [
     {
         positionName: 'PayShare',
         startDate: 'WIP',
-        imageUrl: './resources/logos/PayShare2.jpg',
+        imageUrl: './public/resources/logos/PayShare2.jpg',
         link: ''
     },
     {
     positionName: 'This website',
     startDate: 'July 2023',
-    imageUrl: './resources/logos/logo-shds.jpg',
+    imageUrl: './public/resources/logos/logo-shds.jpg',
     link: '#'
 },
 {
     positionName: 'GuutRecords',
     startDate: 'May 2023',
-    imageUrl: './resources/logos/GuutRecords_nobackground.jpg',
+    imageUrl: './public/resources/logos/GuutRecords_nobackground.jpg',
     link: 'https://guutrecords.netlify.app/'
 },
 {
     positionName: 'QueMePongo!',
     startDate: 'April 2023',
-    imageUrl: './resources/logos/quemepongo-logo2.png',
+    imageUrl: './public/resources/logos/quemepongo-logo2.png',
     link: 'https://quemepongo.netlify.app/'
 },
 ];
